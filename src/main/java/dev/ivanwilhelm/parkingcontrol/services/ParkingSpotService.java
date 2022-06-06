286e4aa4f4f76d77a5fb6b88d6fc09cccad6d409
Ivan Wilhelm <ivan.wilhelm@senior.com.br>
2022-06-06 10:37:51 -0300

    Service repository injection created

--- src/main/java/dev/ivanwilhelm/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/dev/ivanwilhelm/parkingcontrol/services/ParkingSpotService.java
@@ -1,7 +1,13 @@
 package dev.ivanwilhelm.parkingcontrol.services;
 
+import dev.ivanwilhelm.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingSpotService {
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
 }
