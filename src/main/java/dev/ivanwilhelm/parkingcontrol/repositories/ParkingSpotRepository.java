1a48b0e8b11c2e19efdb688dc1034a43d8a31f9d
Ivan Wilhelm <ivan.wilhelm@senior.com.br>
2022-06-06 10:33:22 -0300

    Repository created

---
+++ src/main/java/dev/ivanwilhelm/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,11 @@
+package dev.ivanwilhelm.parkingcontrol.repositories;
+
+import dev.ivanwilhelm.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+}
